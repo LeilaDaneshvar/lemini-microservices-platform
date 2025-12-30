ee8854c8df04db76d6693333a1b644ddde9516a2
Leila Daneshvar <l.daneshvar.tarigh@gmail.com>
2025-12-30 17:33:58 -0500

    feat: init discovery service with eureka server (ref #4)

---
+++ lemini-discovery-service/src/main/java/com/lemini/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.lemini.discovery;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }    
+
+}
---
+++ lemini-discovery-service/src/test/java/com/lemini/discovery/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.lemini.discovery;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+public class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
