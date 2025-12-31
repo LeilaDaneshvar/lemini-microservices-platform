6091edef3e0191d38765b1cbb5c89cef6e70b9f5
Leila Daneshvar <l.daneshvar.tarigh@gmail.com>
2025-12-30 23:15:26 -0500

    feat: init api gateway with eureka discovery client (ref #5)

---
+++ lemini-gateway/src/test/java/com/lemini/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemini.gateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+public class GatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
