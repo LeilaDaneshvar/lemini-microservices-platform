38f5476002eba1ca3b373ef244b4d1cb8a02a6cd
Leila Daneshvar <l.daneshvar.tarigh@gmail.com>
2025-12-31 14:50:30 -0500

    feat: init user service and add vscode launch config (ref #6)

---
+++ lemini-user-service/src/test/java/com/lemini/users/UserApplicationTests.java
@@ -0,0 +1,12 @@
+package com.lemini.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
