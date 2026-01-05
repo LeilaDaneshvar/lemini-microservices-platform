d06fcca85221ad305cbe755b5fa27f835bb0796e
Leila Daneshvar <l.daneshvar.tarigh@gmail.com>
2026-01-05 10:19:33 -0500

    feat: add UserEntity and AddressEntity with Lombok support (ref #9)

---
+++ lemini-user-service/src/main/java/com/lemini/users/io/entity/UserEntity.java
@@ -0,0 +1,48 @@
+package com.lemini.users.io.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String encryptedPassword;
+
+    @Column(nullable = true)
+    private String emailVerificationToken;
+
+    @Column(nullable = false)
+    private Boolean emailVerificationStatus = false;
+
+    @OneToMany(mappedBy = "userProfile", cascade = CascadeType.ALL)
+    private List<AddressEntity> addresses;
+}
