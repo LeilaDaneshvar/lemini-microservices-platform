e88805de8d63bcf429bf42bf1e866d37fdf9e2af
Leila Daneshvar <l.daneshvar.tarigh@gmail.com>
2026-01-20 23:32:07 -0500

    feat: Configure the SecurityFilterChain to permit login and require authentication for other routes. (ref #10)

--- lemini-user-service/src/main/java/com/lemini/users/security/SecurityConstants.java
+++ lemini-user-service/src/main/java/com/lemini/users/security/SecurityConstants.java
@@ -8,6 +8,7 @@ public class SecurityConstants {
 
     public static final long EXPIRATION_TIME = 864_000_000; //10 days
     public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String SIGN_IN_URL = "/users/login";
 
     private static String TOKEN_SECRET;
 
