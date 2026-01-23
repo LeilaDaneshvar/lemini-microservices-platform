ff44f818446b851ddf7d06d1b224f178fe39a074
Leila Daneshvar <l.daneshvar.tarigh@gmail.com>
2026-01-22 22:24:23 -0500

    refactor: Rename PasswordEncoder to PasswordEncodeConfig (ref #10)

--- lemini-user-service/src/main/java/com/lemini/users/security/PasswordEncoderConfig.java
+++ lemini-user-service/src/main/java/com/lemini/users/security/PasswordEncoderConfig.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class PasswordEncoder  {
+public class PasswordEncoderConfig  {
     
-     @Bean
+    @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(10);
     }
 
 
