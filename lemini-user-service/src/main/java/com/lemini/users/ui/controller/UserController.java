2aff4c5ef7762508a920dc7bd3b069ed6c10bb18
Leila Daneshvar <l.daneshvar.tarigh@gmail.com>
2026-01-26 23:20:45 -0500

    feat: Map the GET endpoint to the service and handle response mapping. (ref #11)

--- lemini-user-service/src/main/java/com/lemini/users/ui/controller/UserController.java
+++ lemini-user-service/src/main/java/com/lemini/users/ui/controller/UserController.java
@@ -14,16 +14,21 @@ import com.lemini.users.ui.model.response.ApiErrorResponse;
 import com.lemini.users.ui.model.response.UserRest;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -66,4 +71,37 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(returnValue);
     }
+
+    @Operation(summary = "Get User info by public Id ", 
+                description = "Retrieve user profile information using the public user ID for logged in user",
+                        security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+            // Senario 1: Successful Retrieval
+            @ApiResponse(responseCode = "200", description = "User profile retrieved successfully"),
+
+            // Senario 2: Error
+            @ApiResponse(responseCode = "404", 
+            description = "User Not Found", 
+            content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),
+
+            @ApiResponse(responseCode = "401", 
+            description = "Unauthorized (Invalid or missing authentication token)", 
+            content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+
+    })
+    @GetMapping(path = "{userId}", produces = {
+            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    public ResponseEntity<UserRest> getUser(@Parameter(description = "Public user ID", example = "user123") @PathVariable("userId") String userId) {
+        
+        // Retrieve User DTO
+        UserDto userDto = userService.getUserByUserId(userId);
+
+        // Map DTO to Response Model
+        UserRest returnValue = mapper.userDtoToUserRest(userDto);
+
+        // Return Response
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(returnValue);
+    }
+    
 }
