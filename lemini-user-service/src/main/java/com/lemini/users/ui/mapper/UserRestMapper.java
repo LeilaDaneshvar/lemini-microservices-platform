a0d03cd639730e8f8aaaed039e89cb8eb3ba5410
Leila Daneshvar <l.daneshvar.tarigh@gmail.com>
2026-02-03 17:56:40 -0500

    feat: Add mapping logic to convert UserUpdateRequestModel to UserDto and then to UserEntity. (ref #12)

--- lemini-user-service/src/main/java/com/lemini/users/ui/mapper/UserRestMapper.java
+++ lemini-user-service/src/main/java/com/lemini/users/ui/mapper/UserRestMapper.java
@@ -4,16 +4,23 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 import com.lemini.users.shared.dto.UserDto;
+import com.lemini.users.ui.model.request.UpdateUserRequestModel;
 import com.lemini.users.ui.model.request.UserRequestModel;
 import com.lemini.users.ui.model.response.UserRest;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserRestMapper {
 
-    // Requet to DTO
+    //Requet to DTO
     UserDto userRequestModelToUserDto(UserRequestModel userRequestModel);
 
     //DTO to Response
     UserRest userDtoToUserRest(UserDto userDto);
+
+    //Update Request to DTO
+    UserDto updateUserRequestModelToUserDto(UpdateUserRequestModel updateUserRequestModel);
+
+    //Update DTO to Response
+    UserRest updateUserDtoToUserRest(UserDto userDto);
     
 } 
