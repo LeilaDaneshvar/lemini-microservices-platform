f8cf49c4147a6cbad13cfeb2bc011a16ec6433ae
Leila Daneshvar <l.daneshvar.tarigh@gmail.com>
2026-02-17 17:53:13 -0500

    feat: Adding CRUDRepository for AuthorityEntity and RoleEntity (ref # 16)

--- lemini-user-service/src/main/java/com/lemini/users/io/entity/RoleEntity.java
+++ lemini-user-service/src/main/java/com/lemini/users/io/entity/RoleEntity.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -12,11 +13,14 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "roles")
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
